d8a1d4ea68db1637887adbade0e4b1b9a6f19fe5
Rick Symington <rick.symington@gmail.com>
2018-12-13 14:12:18 +0000

    Added DB & SQL_runner

---
+++ bootcamp/db/bootcamp.sql
@@ -0,0 +1,26 @@
+DROP TABLE bookedclasses;
+DROP TABLE members;
+DROP TABLE gymclasses;
+
+CREATE TABLE members (
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  age INT2,
+  gender VARCHAR(255),
+  membership_category VARCHAR(255)
+);
+
+CREATE TABLE gymclasses(
+  id SERIAL8 primary key,
+  gymclass_name VARCHAR(255),
+  instructor VARCHAR(255),
+  capacity INT8,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP
+);
+
+CREATE TABLE bookedclasses(
+  id SERIAL8 primary key,
+  member_id INT8 REFERENCES members(id),
+  gymclass_id INT8 REFERENCES gymclasses(id)
+);
